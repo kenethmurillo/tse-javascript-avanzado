14a833e937818d38c6a88022046a11c353aa0faa
Keneth.Murillo.76008790 <Keneth.Murillo@microfocus.com>
2022-09-15 17:02:34 -0600

    Ejercicio Resuelto N3

--- Sesiones/Clase 20220914/Ejercicio N3 - Resuelto.js
+++ Sesiones/Clase 20220914/Ejercicio N3 - Resuelto.js
@@ -16,7 +16,36 @@ const carrito = {
     }
     return precio;
   },
-  anade(producto){
-    
+  anade(producto) {
+    const { nombre, unidades = 1, precio } = producto
+
+    if (typeof nombre !== 'string') {
+      throw new Error('No es un texto.');
+    };
+
+    /*if (!Number.isNaN(precio)) {
+      throw new Error('No es un número.');
+    }*/
+
+    if (!Number.isInteger(unidades)) {
+      throw new Error('No es un entero.');
+    }
+
+    const encontrado = this.productos.findIndex(producto =>
+      producto.nombre.toLowerCase().trim() === nombre.toLowerCase().trim());
+
+    if (encontrado) {
+      // Bloque de código para sumar y cambiar el precio.
+      console.log(encontrado);
+    } else {
+      // agregar el nuevo producto.
+      this.productos.push(producto);
+    }
   }
 }
+
+carrito.anade({
+  nombre: "chocolate",
+  unidades: 8,
+  precio: 2
+});
