adb668e3d2cbb1a0ff10ffdbedda7b2d423603c7
Keneth.Murillo.76008790 <Keneth.Murillo@microfocus.com>
2022-10-03 18:31:09 -0600

    Todo App

--- Sesiones/Clase 20220929/React.md
+++ Sesiones/Clase 20220929/React.md
@@ -1,1 +1,32 @@
 React: SPA => Single Page Application
+
+Project Administration
+
+    Gatsby
+        npx gatsby new my-app
+
+    Razzle
+        npm run start:prod
+
+    NextJS
+        npx create-next-app my-app
+
+    Preact
+        npx preact-cli create my-app
+        Reducir el proyecto de React, sin pagar de más.
+
+    Storybook
+
+Testing
+Cypress
+npm install cypress
+
+    Pruebas de UI           -->  Pruebas Funcionales y Manuales
+
+    Pruebas de Integración  --> Probar API o cualquier tipo de integración
+        Pruebas de Load
+        Pruebas de Strike
+
+    Pruebas Unitarias       -->  Desarrollador
+        Code Coverage
+        System.assert()
---
+++ Sesiones/Clase 20221003/react/todo-app/src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div className="todo">
+      <span>{todo.text}</span>
+      <div>
+        <Button variant="success" onClick={() => markTodo(index)}>✓</Button>
+        <Button variant="danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const submitEvt = e => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={submitEvt}>
+      <Form.Group>
+        <Form.Label>Add Todo</Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="Add new todo" ></Form.Control>
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">Add</Button>
+    </Form>
+  );
+}
+
+function App() {
+
+  const [todos, setTodos] = React.useState([{
+    text: 'Tarea de ejemplo',
+    isDone: false
+  }]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    let todo = newTodos[index];
+    todo.isDone = true;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>To-do List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo} />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
